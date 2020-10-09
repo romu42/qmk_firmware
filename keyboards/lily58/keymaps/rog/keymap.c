a268c8a29b7fa918069cff69638904ae83b05bd5
Roger Murray <roger.murray@LM338.local>
2020-10-09 22:16:33 +0200

    moved _ to -

--- keyboards/lily58/keymaps/rog/keymap.c
+++ keyboards/lily58/keymaps/rog/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_LBRC,  KC_RBRC, KC_F9, KC_LBRC, \
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR,  KC_LPRN, KC_RPRN, KC_ASTR, KC_TILD, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, XXXXXXX, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_UNDS, KC_LCBR, KC_RCBR, KC_PIPE, XXXXXXX, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 /* RAISE
